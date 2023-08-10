8273bc5ac7b9f71eb7e2e0d7ee53ee456bd64545
ajaykrupalk <ajaykrupalk@outlook.in>
2023-08-10 22:13:32 +0530

    Added env file

--- frontend/src/app/services/resource.service.ts
+++ frontend/src/app/services/resource.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Resource } from './../interfaces/resource';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
